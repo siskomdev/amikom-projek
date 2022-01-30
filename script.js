d893ab518ed474cce982f450714160df7eed6c56
Rahman NM <trirahman.tech@gmail.com>
2022-01-31 04:36:18 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Javascript Rating Counts Plugin
+wpac_init = window.wpac_init || [];
+wpac_init.push({ widget: 'Rating', id: 21697 });
+(function () {
+    if ('WIDGETPACK_LOADED' in window) return;
+    WIDGETPACK_LOADED = true; //true ubah ke False
+    var mc = document.createElement('script');
+    mc.type = 'text/javascript';
+    mc.async = true;
+    mc.src = 'https://embed.widgetpack.com/widget.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(mc, s.nextSibling);
+})();
